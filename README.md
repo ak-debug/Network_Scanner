7407d4eecbaa0020fd0a9a1442f38db3e40bce2c
Akshit Kathuria <akshitkathuria17@gmail.com>
2023-06-03 13:15:03 +0530

    Added Port Range and Improved Functionality

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Network_Scanner
+# Network Scanner
+
+This is a simple network scanner script written in Python that allows you to perform both host discovery using ARP scan and port scanning using TCP SYN scan. It utilizes the Scapy library for sending and receiving network packets.
+
+## Features
+
+- ARP scan: Discover hosts on the network by sending ARP requests and collecting responses.
+- TCP SYN scan: Scan a target host or domain for open ports by sending TCP SYN packets and analyzing the responses.
+- Flexible input: Specify the target IP address or IP range and port range via command-line arguments or user prompts.
+
+## Requirements
+
+- Python 3.x
+- Scapy library (`pip install scapy`)
+
+## Usage
+
+1. Clone or download the script from the GitHub repository.
+2. Install the required dependencies (`pip install scapy`).
+3. Run the script from the command line using the following options:
+
+```shell
+python network_scanner.py [-h] [-t TARGET] [-p PORTS]
+```
+
+- `-t/--target`: Specify the target IP address or IP range to scan (e.g., `192.168.0.1` or `192.168.0.1/24`).
+- `-p/--ports`: Specify the port range to scan (e.g., `1-100`). If not provided, the script will prompt you to enter the port range.
+
+### Examples
+
+1. Perform an ARP scan on a specific IP range and TCP SYN scan on the specified port range:
+
+```shell
+python network_scanner.py -t 192.168.0.1/24 -p 1-100
+```
+
+2. Perform a TCP SYN scan on a specific IP or domain and port range:
+
+```shell
+python network_scanner.py -t example.com -p 1-1000
+```
+
+3. Run the script without command-line arguments (interactive mode):
+
+```shell
+python network_scanner.py
+```
+
+## Note
+
+- Ensure that you have the necessary permissions to send and receive network packets (superuser/administrator access may be required).
+- Be mindful of the potential legal and ethical implications of scanning networks and hosts without proper authorization. Always seek permission and adhere to applicable laws and regulations.
+
+Feel free to contribute, provide feedback, or report any issues via the GitHub repository. Happy scanning!
--- scanner.py
+++ scanner.py
@@ -1,49 +1,101 @@
-from tabnanny import verbose
+import socket
 import scapy.all as scapy
 import argparse
+import sys
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t","--target",dest="target",help="Target IP/ IP range")
-    options= parser.parse_args()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP/ Domain")
+    parser.add_argument("-p", "--ports", dest="ports", help="Port range (e.g., 1-100)")
+    options = parser.parse_args()
     return options
 
-def scan(ip):
-    arp_request= scapy.ARP(pdst=ip)
+def is_ip_address(target):
+    try:
+        socket.inet_aton(target)
+        return True
+    except socket.error:
+        return False
+
+def is_ip_or_domain(target):
+    try:
+        socket.inet_aton(target)
+        return True
+    except socket.error:
+        try:
+            socket.gethostbyname(target)
+            return True
+        except socket.gaierror:
+            return False
+
+def arp_scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)[0]
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     return answered_list
 
-def print_result(result_list):
-    print("IP\t\t\tMAC ADDRESS\n")
-    for ele in result_list:
-        print(ele[1].psrc+"\t\t"+ele[1].hwsrc)
+def tcp_syn_scan(ip, start_port, end_port):
+    open_ports = []
+    for port in range(start_port, end_port + 1):
+        print(f"Checking port {port}...", end="\r")
+        sys.stdout.flush()
+        ip_packet = scapy.IP(dst=ip)
+        tcp_packet = scapy.TCP(dport=port, flags="S")
+        packet = ip_packet / tcp_packet
+        response = scapy.sr1(packet, timeout=1, verbose=False)
+        if response is not None and response.haslayer(scapy.TCP) and response.getlayer(scapy.TCP).flags == 0x12:
+            open_ports.append(port)
+    print("\n")
+    return open_ports
 
+def print_result(result_list, open_ports):
+    print("IP\t\t\tMAC ADDRESS")
+    for ele in result_list:
+        print(ele[1].psrc + "\t\t" + ele[1].hwsrc)
+
+    if open_ports:
+        print("\nOpen Ports:")
+        for port in open_ports:
+            print(port)
+    else:
+        print("\nNo open ports found.")
 
 if __name__ == "__main__":
-    
     print("Choose the Scan Type")
     print("[1] ARP Scan\n[2] TCP SYN Scan")
-    x=input()
-    if x=="1": 
-        options=get_arguments() 
+    x = input()
+    if x == "1":
+        options = get_arguments()
         if not options.target:
-            options.target=input("Enter ip or ip range\n") 
-        scan_result=scan(options.target)
-        print_result(scan_result)
-
-    elif x=="2":
-        destination=input("Enter Destination's IP or Domain\n")
-        tcp_pack=scapy.TCP(dport=80,flags="S") #http SYN
-        ip_pack=scapy.IP(dst=destination)
-        answer=scapy.sr1(ip_pack/tcp_pack)
-        print(answer.summary())
-    
+            options.target = input("Enter IP or IP range: ")
+        if not is_ip_address(options.target):
+            print("ARP scan requires an IP address or IP range.")
+            exit(0)
+        if not options.ports:
+            options.ports = input("Enter port range (e.g., 1-100): ")
+        port_range = options.ports.split("-")
+        start_port = int(port_range[0])
+        end_port = int(port_range[1])
+        scan_result = arp_scan(options.target)
+        open_ports = tcp_syn_scan(options.target, start_port, end_port)
+        print_result(scan_result, open_ports)
+    elif x == "2":
+        options = get_arguments()
+        if not options.target:
+            options.target = input("Enter Destination's IP or Domain: ")
+        if not is_ip_or_domain(options.target):
+            print("TCP SYN scan requires an IP address or domain name.")
+            exit(0)
+        if not options.ports:
+            options.ports = input("Enter port range (e.g., 1-100): ")
+        port_range = options.ports.split("-")
+        start_port = int(port_range[0])
+        end_port = int(port_range[1])
+        open_ports = tcp_syn_scan(options.target, start_port, end_port)
+        print("Open Ports:")
+        for port in open_ports:
+            print(port)
     else:
         print("No such option")
         exit(0)
-
-
-
-
